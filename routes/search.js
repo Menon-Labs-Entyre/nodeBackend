e96da297a20e51f6e9dcd46d4b309342b80d6282
Mgiar2015 <mgiar2015@gmail.com>
2021-04-05 19:45:33 -0400

    Add routes to search

--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,28 @@
 var express = require('express');
-var
+const drugBank = require('../services/drugBank');
 
 var router = express.Router();
 
 //request list of medications with similar name
-router.get("/medication",(req, res) => {
+router.get("/medication",async(req, res) => {
+    const nameInput = req.query.q;
+    console.log(nameInput);
+    const medications = await drugBank.getMedications(nameInput);
+    res.json(medications);
+})
 
+//request list of medications with similar name
+router.get("/side_effects",async(req, res) => {
+    const idInput = req.query.id;
+    const sideEffects = await drugBank.getMedications(idInput);
+    res.json(sideEffects);
+})
 
+//request list of medications with similar name
+router.get("/conditions",async(req, res) => {
+    const nameInput = req.query.q;
+    const conditions = await drugBank.getConditions(nameInput);
+    res.json(conditions);
 })
 
 module.exports = router;
