4925ac2de9f58030c33388ce7f56633f24929e42
Mgiar2015 <mgiar2015@gmail.com>
2021-04-15 16:19:02 -0400

    Added a few more functions for calling the api, creating patientPackage creation

--- services/drugBank.js
+++ services/drugBank.js
@@ -44,10 +44,78 @@ const getSideEffects = async(drugBankId) => {
   return sideEffects;
 }
 
+
+/* Retrieve details related to drug based on drugBankId */
+/* Includes toxicitiy and clearance */
+const getDrug = async(drugBankId) => {
+  const callEndpoint = "/drugs/"+drugBankId; //using the simple return
+  const response = await axios.get(host+callEndpoint);
+  const drugInfo = response.data;
+  return drugInfo;
+}
+
+
+/* Given a list of drugBankId's  retrieve a list of interactions */
+const getInteractions = async(drugBankIds) => {
+  const callEndpoint = "/ddi" 
+  const callParamters = {
+    params: {
+      drugbank_id: drugBankIds
+    }
+  }
+  const response = await axios.get(host+callEndpoint,callParamters);
+  const interactionData = response.data;
+  return interactionData;
+}
+
+/* Given a condition Id, return the list of assosiated drugs linked to a condition */
+const getDrugsbyCondition = async(conditionId) => {
+  const callEndpoint = "/conditions/" + conditionId + "/drugs" 
+  const callParamters = {
+    params: {
+      more: true
+    }
+  }
+  const response = await axios.get(host+callEndpoint,callParamters);
+  const drugData = response.data;
+  return drugData;
+}
+
+/* Given a condition Id, return the list of assosiated drugs linked to a condition */
+/* could potentially add caching to speed this whole thing up */
+const getIndicationsbyCondition = async(conditionId) => {
+  const callEndpoint = "/conditions/" + conditionId + "/indications" 
+  const callParamters = {
+    params: {
+      more: true
+    }
+  }
+  const response = await axios.get(host+callEndpoint,callParamters);
+  const indiciationData = response.data;
+  return drugData;
+}
+
+const createDrugPackage = async() => {
+  
+}
+
+const createPatientPackage = async() => {
+  const testData = [{conditionId:null,drugBankId:null}]
+  const drugIds = testData.map((diagnosis) => diagnosis.drugBankId);  // add code to make sure t here are no dups
+  const conditionIds = testData.map((diagnosis) => diagnosis.conditionId); // add code to make sure t here are no dups
+  const interactions = getInteractions(drugIds);
+  const drugData = drugIds.map((drugId) => getDrug(drugId)); 
+  const conditionData = conditionIds.map((drugId) => getDrug(drugId)); 
+  return {}
+  
+}
+
 //export methods for use
 module.exports = {
   getMedications: getMedications,
   getConditions: getConditions,
-  getSideEffects: getSideEffects
+  getSideEffects: getSideEffects,
+  getDrug: getDrug,
+  getInteractions: getInteractions
 }
 
