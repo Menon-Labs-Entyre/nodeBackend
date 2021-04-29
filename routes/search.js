b750fd01773bf62f752fb2b2e1872dbd0fbb0522
Mgiar2015 <mgiar2015@gmail.com>
2021-04-29 19:31:17 -0400

    Slight change to comments in search

--- routes/search.js
+++ routes/search.js
@@ -19,6 +19,28 @@ router.get("/condition",async(req, res) => {
     res.json(conditions.slice(0,returnLimit));
 })
 
+//work in progress
+router.get("/condition/filtered",async(req, res) => {
+    const nameInput = req.query.q;
+    const productInput = req.query.product;
+    const returnLimit = req.query.limit;
+    const conditionsP = drugBank.getConditions(nameInput);
+    const indicationsForProduct = drugBank.getIndicationsbyProduct(drugBank.productNameToProduct[productInput].drugbank_pcid)
+    const data =  await Promise.all([conditionsP,indicationsForProduct])
+    const conditionsForProduct = data[1].map(indication => indication.condition.name)
+    const uConditionsForProduct = [ ...new Set(conditionsForProduct)] 
+    let conditions = [] 
+    for(const condition of uConditionsForProduct){
+        if(data[0].includes(condition)){
+            conditions.push(condition);
+        }
+    }
+    if(conditions.length == 0){
+        conditions = data[0]
+    }
+    res.json(conditions.slice(0,returnLimit));
+})
+
 //request list of side_effects with similar name
 router.get("/side_effect",async(req, res) => {
     const nameInput = req.query.q;
