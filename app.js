77ab1d9fe9bad4e543570317715661683c2a5734
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2021-05-02 16:19:01 -0400

    Enable backend to accept cross-domain requests

--- app.js
+++ app.js
@@ -37,6 +37,14 @@ app.use('/', indexRouter);
 app.use('/search',searchRouter)
 app.use('/users', usersRouter);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
