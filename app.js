d5c0f1f74103f5f4df1b5903d91dda6dd32f140a
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2021-05-02 16:41:13 -0400

    Another attempt to fix session

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.use('/search',searchRouter)
 app.use('/users', usersRouter);
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  //res.header('Access-Control-Allow-Origin', req.headers.origin);
+  //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
 
